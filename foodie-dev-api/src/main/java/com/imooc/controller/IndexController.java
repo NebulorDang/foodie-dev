eb3123c5cf391aed543870539a1d39203c96dc37
polpol <369671231@qq.com>
2022-10-29 11:15:55 +0800

    stage-1-week2-1-1 carousel

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/IndexController.java
@@ -0,0 +1,34 @@
+package com.imooc.controller;
+
+import com.imooc.enums.YesOrNo;
+import com.imooc.pojo.Carousel;
+import com.imooc.service.CarouselService;
+import com.imooc.utils.IMOOCJSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("index")
+@Api(value = "首页", tags = {"首页展示的相关接口"})
+public class IndexController {
+
+    @Autowired
+    private CarouselService carouselService;
+
+    @ApiOperation(value = "获取首页轮播图列表", notes = "获取首页轮播图列表", httpMethod = "GET")
+    @GetMapping("/carousel")
+    public IMOOCJSONResult carousel() {
+        List<Carousel> result = carouselService.queryAll(YesOrNo.YSE.type);
+        return IMOOCJSONResult.ok(result);
+    }
+}
