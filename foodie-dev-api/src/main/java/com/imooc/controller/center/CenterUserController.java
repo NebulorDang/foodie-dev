f86ffbe6d8e7bc3836d54b62145269a9f3d73e46
polpol <369671231@qq.com>
2022-11-04 16:22:05 +0800

    stage1-week4-2-2 upload avatar

--- foodie-dev-api/src/main/java/com/imooc/controller/center/CenterUserController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/center/CenterUserController.java
@@ -1,5 +1,6 @@
 package com.imooc.controller.center;
 
+import com.imooc.controller.BaseController;
 import com.imooc.pojo.Users;
 import com.imooc.pojo.bo.center.CenterUserBO;
 import com.imooc.service.center.CenterUserService;
@@ -9,14 +10,21 @@ import com.imooc.utils.JsonUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,7 +32,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("userInfo")
 @Api(value = "用户信息接口", tags = {"用户信息页面的API接口"})
-public class CenterUserController {
+public class CenterUserController extends BaseController {
 
     @Autowired
     private CenterUserService centerUserService;
@@ -56,6 +64,74 @@ public class CenterUserController {
         return IMOOCJSONResult.ok();
     }
 
+    @ApiOperation(value = "修改用户头像", notes = "修改用户头像", httpMethod = "POST")
+    @PostMapping("uploadFace")
+    public IMOOCJSONResult uploadFace(
+            @ApiParam(name = "userId", value = "用户id", required = true)
+            @RequestParam String userId,
+            @ApiParam(name = "file", value = "用户头像", required = true)
+            MultipartFile file,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        // 定义头像保存的地址
+        String fileSpace = IMAGE_USER_FACE_LOCATION;
+        // 在路径上为每一个用户增加一个子文件夹，用userId区分
+        String uploadPathPrefix = File.separator + userId;
+
+        // 开始文件上传
+        if (file != null) {
+
+            FileOutputStream fileOutputStream = null;
+            try {
+                // 获得文件上传的文件名称
+                String fileName = file.getOriginalFilename();
+
+                if (StringUtils.isNotBlank(fileName)) {
+                    // 文件重命名 imooc-face.png -> ["imooc-face", "png"]
+                    String fileNameArr[] = fileName.split("\\.");
+
+                    //获取文件的后缀名
+                    String suffix = fileNameArr[fileNameArr.length - 1];
+
+                    // face-{userid}.png
+                    // 文件名称重组 覆盖式上传，增量式（拼接当前时间）
+                    String newFileName = "face-" + userId + "." + suffix;
+
+                    // 上传的头像最终保存的位置
+                    String finalFacePath = fileSpace + uploadPathPrefix + File.separator + newFileName;
+
+                    File outFile = new File(finalFacePath);
+                    if (!outFile.getParentFile().exists()) {
+                        // 创建文件夹
+                        outFile.getParentFile().mkdirs();
+                    }
+
+                    // 文件出输出保存到目录
+                    fileOutputStream = new FileOutputStream(outFile);
+                    InputStream inputStream = file.getInputStream();
+                    IOUtils.copy(inputStream, fileOutputStream);
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (fileOutputStream != null) {
+                    try {
+                        fileOutputStream.flush();
+                        fileOutputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } else {
+            return IMOOCJSONResult.errorMsg("文件不能为空！");
+        }
+
+        return IMOOCJSONResult.ok();
+    }
+
     private void setNullProperty(Users userResult) {
         userResult.setPassword(null);
         userResult.setMobile(null);
@@ -67,7 +143,7 @@ public class CenterUserController {
 
     private Map<String, String> getErros(BindingResult result) {
         Map<String, String> map = new HashMap<>();
-        List<FieldError> errorList =  result.getFieldErrors();
+        List<FieldError> errorList = result.getFieldErrors();
         for (FieldError error : errorList) {
             // 发生验证错误所对应的某一个属性
             String errorField = error.getField();
