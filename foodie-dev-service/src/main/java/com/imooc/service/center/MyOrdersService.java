6dea17ebae1da8e2406b637c7c20b27fa7a2943f
polpol <369671231@qq.com>
2022-11-07 16:14:14 +0800

    stage1-week4-3-7 order management

--- foodie-dev-service/src/main/java/com/imooc/service/center/MyOrdersService.java
+++ foodie-dev-service/src/main/java/com/imooc/service/center/MyOrdersService.java
@@ -1,5 +1,6 @@
 package com.imooc.service.center;
 
+import com.imooc.pojo.Orders;
 import com.imooc.utils.PagedGridResult;
 
 public interface MyOrdersService {
@@ -16,4 +17,34 @@ public interface MyOrdersService {
                                          Integer orderStatus,
                                          Integer page,
                                          Integer pageSize);
+
+    /**
+     * 将订单状态改为商家发货
+     * @param orderId
+     */
+    public void updateDeliverOrderStatus(String orderId);
+
+    /**
+     * 查询我的订单
+     * @param userId
+     * @param orderId
+     * @return
+     */
+    public Orders queryMyOrder(String userId, String orderId);
+
+    /**
+     * 更新订单状态
+     * @param orderId
+     * @return
+     */
+    public boolean updateReceiveOrderStatus(String orderId);
+
+    /**
+     * 删除订单 （逻辑删除）
+     * @param userId
+     * @param orderId
+     * @return
+     */
+    public boolean deleteOrder(String userId, String orderId);
+
 }
