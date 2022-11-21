4b5c73f21d8a17bcfc2f03e5c3de71092ce124c8
polpol <369671231@qq.com>
2022-11-21 11:51:52 +0800

    stage3-week9-1-6 SpringSession using redis

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -2,16 +2,20 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import tk.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication
+//不使用spring-session安全登录的密码
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 // 扫描mybatis通用mapper所在的包
 @MapperScan(basePackages = "com.imooc.mapper")
 // 扫描所有包以及相关组件包
 @ComponentScan(basePackages = {"com.imooc", "org.n3r.idworker"})
 @EnableScheduling
+@EnableRedisHttpSession // 开启使用redis作为spring session
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
