faee3896160694ad94e49b6c631497924ddd847a
polpol <369671231@qq.com>
2022-11-21 21:26:57 +0800

    stage3-week9-3-11 CAS(Central Authentication Service) SSO(Single Sign On)

---
+++ foodie-sso/src/main/java/com/imooc/Application.java
@@ -0,0 +1,18 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
+
+//不使用spring-session安全登录的密码
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+// 扫描mybatis通用mapper所在的包
+@MapperScan(basePackages = "com.imooc.mapper")
+@ComponentScan(basePackages = {"com.imooc", "org.n3r.idworker"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
