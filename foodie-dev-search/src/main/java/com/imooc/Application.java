5fca54fd29ec210c62c6bcb3d628846da0fae0ca
polpol <369671231@qq.com>
2022-11-25 13:34:14 +0800

    stage3-week10-8-5 use es to highlight in searching with keyword

--- foodie-dev-search/src/main/java/com/imooc/Application.java
+++ foodie-dev-search/src/main/java/com/imooc/Application.java
@@ -2,8 +2,10 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+//不使用spring-session安全登录的密码
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
